c5c135499480127befe89427e874d0bdf1a26dbd
jakob <jakobbarnes05@gmail.com>
2022-07-03 01:18:30 -0500

    Initial commit

---
+++ src/main/java/net/bobyjones/epicserveradditions/EpicServerAdditions.java
@@ -0,0 +1,10 @@
+package net.bobyjones.epicserveradditions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EpicServerAdditions implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
